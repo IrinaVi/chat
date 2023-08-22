7344fa8eb88e186368700412dc19bc4b7713854c
IrinaVi <IrinaVi>
2023-08-22 15:56:41 +0100

    Add gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -7,6 +9,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+
+  try {
+    const r = await axios.put (
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": process.env.KEY}}
+    )
+  } catch (e) {
+
+  }
   return res.json({ username: username, secret: "sha256..." });
 });
 
