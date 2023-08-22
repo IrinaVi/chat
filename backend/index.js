9d91ab28d164c86b7097144aa9c8fbb7a1a2a4f2
IrinaVi <IrinaVi>
2023-08-22 15:59:05 +0100

    Return status and data

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,10 @@ app.post("/authenticate", async (req, res) => {
         {username: username, secret: username, first_name: username},
         {headers: {"private-key": process.env.KEY}}
     )
+    return res.status(r.status).json(r.data)
   } catch (e) {
-
+    return res.status(e.response.status).json(r.response.data)
   }
-  return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
