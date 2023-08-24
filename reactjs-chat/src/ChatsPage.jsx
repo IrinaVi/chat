720226defd02465b8cd18154c02265617256bdb4
IrinaVi <IrinaVi>
2023-08-24 13:13:42 +0100

    Add chat rendering

--- reactjs-chat/src/ChatsPage.jsx
+++ reactjs-chat/src/ChatsPage.jsx
@@ -1,5 +1,13 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine';
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(process.env.KEY, props.user.username, props.user.secret)
+    return (
+        <div style={{ height: '100vh' }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    )
 }
 
 export default ChatsPage;
